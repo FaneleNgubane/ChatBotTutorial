36b7c9c3c28d184bafebb83a1d794da7dca9122c
Fanele <fngubane023a@student.wethinkcode.co.za>
2024-03-14 14:10:05 +0200

    added training data

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,5 +1,5 @@
 import nltk
-
+import numpy as np
 
 from nltk.stem.porter import PorterStemmer    
 stemmer = PorterStemmer()
@@ -11,9 +11,17 @@ def stem(word):
     return stemmer.stem(word.lower())
 
 def bag_of_words(tokenized_sentence, all_words):
-    pass
 
-words = ["organize","organizes","organizing"]
+    """
+    sentence = ["hello", "how", "are", "you"]
+    words = ["hi","hello","I","you","bye","thank","cool"]
+    bog = [   0,    1,    0,   1,    0,    0,      0]
+    """
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
 
-stem_words = [stem(w) for w in words]
-print(stem_words)
+    bag = np.zeros(len(all_words), dtype=np.float32)
+    for idx, w, in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[idx] = 1.0
+    return bag
+
